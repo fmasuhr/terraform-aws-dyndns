3e3f5dee66d4200562034dbc040b158db3ee0ae7
Fionn Masuhr <git@fionn.io>
2020-07-03 08:46:38 +0200

    relax version constraints

--- examples/basic/versions.tf
+++ examples/basic/versions.tf
@@ -2,7 +2,8 @@ terraform {
   required_version = "~> 0.12"
 
   required_providers {
-    aws    = "~> 2"
-    random = "~> 2"
+    archive = "~> 1"
+    aws     = "~> 2"
+    random  = "~> 2"
   }
 }
--- versions.tf
+++ versions.tf
@@ -1,8 +1,8 @@
 terraform {
-  required_version = "~> 0.12.26"
+  required_version = "~> 0.12"
 
   required_providers {
-    aws     = "~> 2.64"
-    archive = "~> 1.3"
+    archive = "~> 1"
+    aws     = "~> 2"
   }
 }
